5f2a6f53a947b3fb19dfbfb17acfc1f0364ac6fb
jessshu <jessica.minhu@gmail.com>
2023-08-02 23:21:22 +0930

    prac2-1-4 debug

--- function-1-4.cpp
+++ function-1-4.cpp
@@ -3,7 +3,7 @@
 void print_scaled(int array[3][3],int scale) {
     for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 3; j++) {
-            array[i][j] = array[i][j] * 3;
+            array[i][j] = array[i][j] * scale;
         }
     }
 
