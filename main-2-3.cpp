addcbe5b20ef6afb843abdfb15bbdce4a394dcda
jessshu <jessica.minhu@gmail.com>
2023-08-03 13:45:45 +0930

    prac2-2-3 change test

--- main-2-3.cpp
+++ main-2-3.cpp
@@ -1,11 +1,9 @@
 #include <iostream>
-// extern bool is_palindrome(int integers[], int length);
-// extern int sum_array_elements(int integers[], int length);
 extern int sum_if_palindrome(int integers[], int length);
 
 int main() {
-    int len = 5;
-    int nums[5] = {1,2,3,2,1};
+    int len = 3;
+    int nums[3] = {1,2,1};
 
     int sum = sum_if_palindrome(nums,len);
 
