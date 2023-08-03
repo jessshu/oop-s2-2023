09db83f093ab31b25307ce80a880b9a2e235f919
jessshu <jessica.minhu@gmail.com>
2023-08-03 13:30:27 +0930

    prac2-2-3 debug

--- function-2-3.cpp
+++ function-2-3.cpp
@@ -17,6 +17,10 @@ bool is_palindrome(int integers[], int length) {
 
 
 int sum_array_elements(int integers[], int length) {
+    if (length < 0) {
+        return false; 
+    }
+
     int sum = 0; 
 
     for (int i = 0; i < length; i++) {
@@ -29,6 +33,10 @@ int sum_array_elements(int integers[], int length) {
 
 int sum_if_palindrome(int integers[], int length) {
     int sum = 0;
+    
+    if (length < 0) {
+        return false; 
+    }
 
     if (is_palindrome(integers,length) == true) {
         // call sum function
