477fb78b4525ea90ce365e69f4460ac0f9d101d6
jessshu <jessica.minhu@gmail.com>
2023-08-17 15:02:49 +0930

    prac3 fix again

--- function-1-3.cpp
+++ function-1-3.cpp
@@ -3,13 +3,15 @@
 PersonList deepCopyPersonList(PersonList pl) {
     PersonList deep;
 
+    deep.people = new Person[pl.numPeople];
+
     for (int i = 0; i < pl.numPeople; i++) {
         deep.people[i].name = pl.people[i].name;
         deep.people[i].age = pl.people[i].age;
     }
     
     deep.numPeople = pl.numPeople;
-    
+
     return deep;
 
 }
