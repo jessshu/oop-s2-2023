0c14e27a8e083986184a047bb9cfadd5e7371bff
jessshu <jessica.minhu@gmail.com>
2023-08-17 14:56:11 +0930

    prac4 fix1

--- main-1-4.cpp
+++ main-1-4.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include "Person.h"
-extern PersonList createPersonList(int n);
+extern PersonList shallowCopyPersonList(PersonList pl);
 
 int main() {
     int len = 3;
-    PersonList NewList = createPersonList(len);
+    PersonList NewList;
+
+    PersonList shallowList = shallowCopyPersonList(NewList);
 
     for (int i = 0; i < len; i++) {
         std::cout << NewList.people[i].name << std::endl;
