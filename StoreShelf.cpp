d40c2031253867ffcd8bc9a7f50bf49db327a756
jessshu <jessica.minhu@gmail.com>
2023-08-24 14:51:16 +0930

    L

--- StoreShelf.cpp
+++ StoreShelf.cpp
@@ -4,7 +4,7 @@
 
 StoreShelf::StoreShelf(): width(0) {};                     // default constructor
 StoreShelf::StoreShelf(int width):width(width), curr_boxes(0) {
-    boxes = new MusicBox[curr_boxes];
+    boxes = new MusicBox[width];
 }           // constructor for shelf with given width in centimetres
 
 int StoreShelf::get_width(){
@@ -24,17 +24,17 @@ MusicBox* StoreShelf::get_contents(){
 // returns true and adds music box to shelf if there is sufficient space
 // otherwise returns false
 bool StoreShelf::add_music_box(MusicBox a_music_box){
-    int total_box_width = 0;
-    int counter = 0; 
+    int total_box_width = 0; 
+
+    int box_width = a_music_box.get_width();
 
     for (int i = 0; i < curr_boxes; i++) {
-        total_box_width = width - StoreShelf::boxes[i].get_width();
+        total_box_width = width + box_width;
     }
 
     for (int j = 0; j < width; j++) {
         if (total_box_width < width) {
-            boxes[counter] = a_music_box;
-            counter++;
+            boxes[curr_boxes] = a_music_box;
             curr_boxes++;
             return true;
         } else {
