9716190e1ab17a22358b3c257791535c0e14c657
jessshu <jessica.minhu@gmail.com>
2023-08-24 14:51:52 +0930

    l

--- main-1-2.cpp
+++ main-1-2.cpp
@@ -9,10 +9,5 @@ int main() {
     MusicBox m2("Two", 1);
     MusicBox m3("Three",1);
 
-    // test to see if they work
-    if (Shelf.get_width() == 2) {
-        std::cout << "The width of the shelf is: " << Shelf.get_width() << std::endl;
-    };
-
     return 0;
 }
