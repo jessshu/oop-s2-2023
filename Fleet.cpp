4380507c8dc805658f7ee62224ac6db0f208c803
jessshu <jessica.minhu@gmail.com>
2023-09-07 14:24:58 +0930

    surely this works

--- Fleet.cpp
+++ Fleet.cpp
@@ -5,11 +5,21 @@
 
 Fleet::Fleet() {
     fleet = new Car*[5];
-    // fleet[0] = Tesla*;
-    // fleet[1] = Ford*(1, 20000);
-    // fleet[2] = Car*(30000);
-    // fleet[3] = Ford*(2, 40000); 
-    // fleet[4] = Tesla*('T', 50000);
+
+    Tesla c1('S', 10000);
+    Ford c2(1, 20000);
+    Car c3(30000);
+    Ford c4(2, 40000); 
+    Tesla c5('T', 50000);
+
+    fleet[0]= &c1;
+    fleet[1]= &c2; 
+    fleet[2]= &c3; 
+    fleet[3] = &c4;
+    fleet[4] = &c5;
+
+
+
 }
 
 Car **Fleet::get_fleet() {
---
+++ main-3-1.cpp
@@ -0,0 +1,12 @@
+#include "Fleet.h"
+#include "Car.h"
+#include "Tesla.h"
+#include "Ford.h"
+#include <iostream> 
+
+int main() {
+
+    Ford f1(1,20000);
+    
+    return 0;
+}
