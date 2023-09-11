359fc057bfe50fb31d2e811bf343f496762c89e3
jessshu <jessica.minhu@gmail.com>
2023-09-11 14:12:09 +0930

    idk what this was for

--- Tesla.cpp
+++ Tesla.cpp
@@ -1,49 +1,41 @@
-#include "Car.h"
 #include "Tesla.h"
 
+#include "Car.h"
+
 Tesla::Tesla() {}
-Tesla::Tesla(char model, int price) : Car(price), model(model), batteryPercentage(100) {
-        emissions = 0;
+Tesla::Tesla(char model, int price)
+    : Car(price), model(model), batteryPercentage(100) {
+  emissions = 0;
 }
 
 void Tesla::chargeBattery(int mins) {
-    if (batteryPercentage+(mins*0.5) > 100) {
-        batteryPercentage = 100;
-    } else {
-        batteryPercentage = batteryPercentage + (0.5*mins);
-    }
+  if (batteryPercentage + (mins * 0.5) > 100) {
+    batteryPercentage = 100;
+  } else {
+    batteryPercentage = batteryPercentage + (0.5 * mins);
+  }
 }
 
 void Tesla::drive(int kms) {
-    if (emissions + (kms*74.0) > 37000-1) {
-        emissions = 37000;
-    } else {
-        emissions = emissions + (kms*74.0);
-    }
+  if (emissions + (kms * 74.0) > 37000 - 1) {
+    emissions = 37000;
+  } else {
+    emissions = emissions + (kms * 74.0);
+  }
 
-    if (batteryPercentage - (kms/5) > 0) {
-        batteryPercentage = batteryPercentage - (kms/5.0);
-    } else {
-        batteryPercentage = 0;
-    }
-    
+  if (batteryPercentage - (kms / 5) > 0) {
+    batteryPercentage = batteryPercentage - (kms / 5.0);
+  } else {
+    batteryPercentage = 0;
+  }
 }
 
+char Tesla::get_model() { return model; }
 
-char Tesla::get_model() {
-    return model;
-}
+void Tesla::set_model(char new_model) { model = new_model; }
 
-void Tesla::set_model(char new_model) {
-    model = new_model;
-}
-
-float Tesla::get_batteryPercentage() {
-    return batteryPercentage;
-}
+float Tesla::get_batteryPercentage() { return batteryPercentage; }
 
 void Tesla::set_batteryPercentage(float new_batteryPercentage) {
-    batteryPercentage = new_batteryPercentage;
+  batteryPercentage = new_batteryPercentage;
 }
-
-
