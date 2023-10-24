c4c5e1c96581fe4d856aadd0b5a97c93ddc1db10
jessshu <jessica.minhu@gmail.com>
2023-10-24 16:54:05 +1030

    q1 fix

--- Utils.h
+++ Utils.h
@@ -28,7 +28,7 @@ public:
 
     static double calculateDistance(std::tuple<int,int> pos1, std::tuple<int, int> pos2) {
         // equation : sqrt((x2-x1)^2+(y2-y1)^2)
-        double distance = sqrt(((std::get<0>(pos2) - std::get<0>(pos1))^2) - ((std::get<1>(pos2) - std::get<1>(pos1)^2)) );
+        double distance = sqrt(pow((std::get<0>(pos2) - std::get<0>(pos1)),2) - pow((std::get<1>(pos2) - std::get<1>(pos1)),2));
 
         return distance; 
     };
