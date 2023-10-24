503f899a4fbd462f38aca0a9c6bdd0fc7c58a076
jessshu <jessica.minhu@gmail.com>
2023-10-24 22:37:39 +1030

    q2 main file

---
+++ main-2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <tuple>
+#include "GameEntity.h"
+#include "Effect.h"
+#include "Utils.h"
+#include "Explosion.h"
+#include "Ship.h"
+#include "Mine.h"
+
+int main() {
+    Explosion expl(10,10); 
+    Ship ship(3,3); 
+
+
+    return 0; 
+}
