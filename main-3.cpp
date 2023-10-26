32dffb0739cab540e07f699cf2beaf595efccab8
jessshu <jessica.minhu@gmail.com>
2023-10-26 14:02:42 +1030

    main-3

---
+++ main-3.cpp
@@ -0,0 +1,14 @@
+#include <iostream> 
+#include <tuple>
+
+#include "Spot.h"
+#include "Influence.h"
+#include "Assists.h"
+#include "Snare.h"
+#include "Persona.h"
+#include "Play.h"
+
+int main() {
+
+    return 0; 
+}
