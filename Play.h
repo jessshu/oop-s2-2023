a49f5452188e63ad1faba3f6a0d3552673606230
jessshu <jessica.minhu@gmail.com>
2023-10-26 14:14:52 +1030

    ideke bruh

--- Play.h
+++ Play.h
@@ -42,6 +42,7 @@ public:
     void playCycle(int maxCycles, double snareTriggerDistance) {
         int index = 0; 
 
+
         // determine how big the matrix is 
 
         while (index < maxCycles) {
@@ -64,7 +65,7 @@ public:
 
                 if (std::get<0>(persona_loc) > matrix.size() || std::get<1>(persona_loc) > matrix.size()) {
                     std::cout << "Person has won the game!" << std::endl; 
-                    break; 
+                    index = maxCycles;  
                 }
 
             }
